c712f5b5e8821295c79a0a2b028045b2d618c877
Trover <74741960+Troversones@users.noreply.github.com>
2024-09-25 15:44:34 +0200

    hazi feladat
    unfinished

---
+++ feladat.py
@@ -0,0 +1,18 @@
+#{sor}. sor,{oldal} {szam}. szek
+
+oldal = "jobb"
+index = 0
+indexPerNumber = 0
+
+
+def szekem(azonosito){
+    while(0):
+        if index % 8 == 0:
+            oldal = "bal"
+            indexPerNumber += 1
+        elif indexPerNumber % 2:
+            
+
+
+        index += 1
+}
