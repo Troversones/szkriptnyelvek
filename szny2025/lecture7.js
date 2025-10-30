e5cb65a5bd996b9294192f8c68a348532b4d74c2
Irinyi Kabinet Felhasználó <valaki@pc22202-9.inf.u-szeged.hu>
2025-10-30 12:31:20 +0100

    comment addition

--- szny2025/lecture7.js
+++ szny2025/lecture7.js
@@ -2,7 +2,7 @@ console.log(Math.PI);
 console.log(Math.round(5.6)); //default kerekítés
 console.log(Math.floor(5.6)); //felfele kerekítés
 console.log(Math.ceil(5.1)); //lefele kerekítés
-console.log(Math.sqrt(25)); //square root xd
+console.log(Math.sqrt(25)); //square root xd , melyi szám négyzete ez a szám ezesetben 5
 console.log(Math.pow(25,3)); //25 on the pow(er) of 3 if you catch my drift 25^3
 console.log(Math.asin(1)); 
 console.log(Math.floor(Math.random()*10+1)); //1-10 közötti szám
